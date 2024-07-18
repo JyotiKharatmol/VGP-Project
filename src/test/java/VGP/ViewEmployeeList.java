cf4bca847d48c9c67012e3767bdd12d5c3754a52
JyotiKharatmol <jyotikharatmol3095@gmail.com>
2024-07-18 15:23:34 +0530

    develop branch - second commit

--- src/test/java/VGP/ViewEmployeeList.java
+++ src/test/java/VGP/ViewEmployeeList.java
@@ -1,5 +1,19 @@
 package VGP;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
 public class ViewEmployeeList {
 
+	
+	@Test
+	public void viewEmployeeList()
+	
+	{
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com/");
+		driver.close();
+	}
+	
 }
